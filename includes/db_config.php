d6944dcdb6b74f6d81f19b6adeadbd353d59b3c1
lovelynparis <2145338@slu.edu.ph>
2018-06-11 17:24:36 +0800

    Database connection config file

---
+++ includes/db_config.php
@@ -0,0 +1,8 @@
+<?php
+    $databaseHost = 'phpmyadmin.nmscreative.com';
+    $databaseName = 'ojt_20180611142657';
+    $databaseUsername = 'u20180611142657';
+    $databasePassword = '20180611142657';
+
+    $mysqli = mysqli_connect($databaseHost, $databaseName, $databaseUsername, $databasePassword, $databaseName);
+?>
