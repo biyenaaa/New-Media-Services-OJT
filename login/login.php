3d1ffc94f42ba03b9c6b9166398b2e234b05b020
Aika <aikaviend@gmail.com>
2018-06-13 17:33:02 +0800

    Separated the html log-in and the log-in process and made some changes.

--- login/login.php
+++ login/login.php
@@ -1,31 +1,3 @@
-<?php
-	include "includes/db_config.php";
-	include "includes/db.php";
-
-	session_start();
-	$error = "";
-
-	if($_SERVER["REQUEST_METHOD"] === "POST") {
-		if(empty($_POST['username']) || empty($_POST['password'])) {
-			$error = "Username or Password is incorrect.";
-			echo $error;
-		} else {
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-
-			$db =  new Db();
-			$result = $db->query("SELECT * FROM accounts WHERE username='$username' AND password=md5('$password');");
-			if($result) {
-				header("location: index.php");
-			} else {
-				$error = "User not found.";
-				echo $error;
-			}
-		}
-	}
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -34,17 +6,55 @@
 <body>
 <h1>Blog&White</h1>
 
-<form action="loginprocess.php">
+<form class="form-login" action="loginprocess.php" method="POST">
 	<div class="container">
 		<center>
 			<label for="uname">Username</label>
-			<input type="text" placeholder="Enter Username" name="uname" required><br/>
+			<input type="text" name="username" required><br/>
 
 			<label for="psw">Password</label>
-			<input type="password" placeholder="Enter Password" name="psw" required><br/>
-
+			<input type="password" name="password" required><br/>
+			<?PHP
+				if (isset($_SESSION['errmsg'])) {
+					echo $_SESSION['errmsg'];
+					unset($_SESSION['errmsg']);
+				}else {
+					echo "";
+				}
+			?>
 			<button type="submit">Sign-in</button><br/>
-				<center class="psw">Do not have an account? <a href="localhost:PORT/registration">Sign-up</a></center>
+				<center class="psw">Don't have an account? <a href="localhost/New-Media-Services-OJT/registration.php">Sign-up</a></center>
+		</center>
+	</div>
+</form>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog&White</title>
+</head>
+<body>
+<h1>Blog&White</h1>
+
+<form class="form-login" action="loginprocess.php" method="POST">
+	<div class="container">
+		<center>
+			<label for="uname">Username</label>
+			<input type="text" name="username" required><br/>
+
+			<label for="psw">Password</label>
+			<input type="password" name="password" required><br/>
+			<?PHP
+				if (isset($_SESSION['errmsg'])) {
+					echo $_SESSION['errmsg'];
+					unset($_SESSION['errmsg']);
+				}else {
+					echo "";
+				}
+			?>
+			<button type="submit">Sign-in</button><br/>
+				<center class="psw">Don't have an account? <a href="localhost/New-Media-Services-OJT/registration.php">Sign-up</a></center>
 		</center>
 	</div>
 </form>
