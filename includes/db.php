f2ab76c9ac68edf4a3e99641f00d5b0416fe4248
Aika <aikaviend@gmail.com>
2018-06-13 12:52:26 +0800

    File for the initialization of the DB class and its functions.

---
+++ includes/db.php
@@ -0,0 +1,33 @@
+<?php
+if (!class_exists('Db')) {
+	class Db{
+		private function connect() {
+			global $dbconfig;
+
+			$connection = new mysqli($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $dbconfig['db']);
+
+			if(mysqli_connect_errno()) {
+				printf("Database connection error.");
+				exit();
+			} else {
+				return $connection;
+			}
+		}
+
+		public function query($query){
+			$db = $this->connect();
+
+			$result = $db->query($query);
+
+			return $result;
+		}
+
+		public function insert($query) {
+			$db = $this->connect();
+
+			$result = $db->query($query);
+
+			return $result;
+		}
+	}
+}
