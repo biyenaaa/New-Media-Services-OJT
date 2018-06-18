24cecbe48ec1a636dcd8c23d856127808cdae56d
chaserylle <oneandonlychasey@gmail.com>
2018-06-18 17:04:40 +0800

    #11 logout feature

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+
+	$_SESSION = array();
+
+	if(ini_get("session.user_coockies")){
+		$params = session_get_cookie_params();
+	    setcookie(session_name(), '', time() - 42000,
+	        $params["path"], $params["domain"],
+	        $params["secure"], $params["httponly"]
+	    );
+	}
+
+	session_destroy();
+	header("Location:index.php");
+	die();
+
+?>
