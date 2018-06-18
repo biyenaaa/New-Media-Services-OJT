e62e70047dad08ae198b6b2994e0d9ca5aadd886
Aika <aikaviend@gmail.com>
2018-06-18 17:15:53 +0800

    #32 Initial master panel, basically straight to displaying authors.

--- admin/homepage.php
+++ admin/homepage.php
@@ -1,6 +1,19 @@
 
 <?php 
-     require_once "../includes/loginprocess.php";
+     //require_once "../includes/loginprocess.php";
+	include "../includes/db_config.php";
+	include "../includes/db.php";
+
+	session_start();
+
+	$db = new Db();
+	$author = $db->query("SELECT username, date_registered FROM accounts ORDER BY date_registered DESC;");
+	while($row = mysqli_fetch_array($author)){
+		$username = $row['username'];
+		$date = $row['date_registered'];
+
+		$output = '<div> '.$username.' joined '.$date.' </div>';
+	}
 ?> 
 
 <html>
@@ -8,14 +21,20 @@
 		<title> Admin Homepage </title>
 		<link rel="stylesheet" type="text/css" href="../style/Admin.css">
 	</head>
-	<body>
-		<div class="status_box">
-			<div class="status_box_head">Accounts</div>
-			<div class="status_box_content">
-				Working Accounts: 
-				<br/>
-				Disabled Accounts:
-			</div> 
-		</div>
-	</body>
+<body>
+<ul class="top-nav">
+	<a href="index.php">Home</a>
+	<a href="login/login.php">Sign-in</a>
+	<a href="registration/register.php">Sign-up</a>
+</ul>
+
+<ul class="side-nav">
+	<a href="authors.php">Authors</a>
+	<a href="blogs.php">Blogs</a>
+	<a href="comments.php">Comments</a>
+</ul>
+
+<?php print("$output"); ?>
+
+</body>
 </html>
