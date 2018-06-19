edf1d1232cea6dac2fd337379ba194647a243873
chaserylle <oneandonlychasey@gmail.com>
2018-06-19 16:31:59 +0800

    #35 publish or unpublish bogs functionality -Admin

--- admin/blogs.php
+++ admin/blogs.php
@@ -1,27 +1,3 @@
-
-<?php 
-	include "../includes/db_config.php";
-	include "../includes/db.php";
-	require_once "../includes/session.php";
-
-	// checks if the user logged in is an admin account
-	if($acc_type!=1){
-		header("Location:../login/login.php");
-        exit;
-	}
-
-
-	$db = new Db();
-	$blog = $db->query("SELECT p.title, p.date_published, a.username FROM posts as p NATURAL JOIN accounts as a ORDER BY date_published DESC;");
-	while($row = mysqli_fetch_array($blog)){
-		$title = $row['title'];
-		$username = $row['username'];
-		$date = $row['date_published'];
-
-		$output = '<div> '.$title.' by '.$username.' '.$date.' </div>';
-	}
-?> 
-
 <html>
 	<head>
 		<title> Admin Homepage </title>
@@ -40,7 +16,36 @@
 	<a href="comments.php">Comments</a>
 </ul>
 
-<?php print("$output"); ?>
+<?php 
+	include "../includes/db_config.php";
+	include "../includes/db.php";
+	require_once "../includes/session.php";
+
+	// checks if the user logged in is an admin account
+	if($acc_type!=1){
+		header("Location:../login/login.php");
+        exit;
+	}
+
+	$db = new Db();
+	$blogs = $db->query("SELECT posts.post_id, accounts.username, posts.title, (DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW()) AS days FROM posts INNER JOIN accounts ON posts.acc_id=accounts.acc_id;");
+
+	while($rowcom = mysqli_fetch_array($blogs)) {
+				$post_id = $rowcom['post_id'];
+				$username = $rowcom['username'];
+				$title = $rowcom['title'];
+				$days = $rowcom['days'];
+
+				$output = '<tr> '.$title.' <i> '.$days.' day/s ago by '.$username.'</i> <tr>';
+				
+				echo '<div class="content"><table>';
+				echo '<form action="publish.php" method="get">';
+					print("$output");
+				echo '<input type="hidden" name="postId" value="'.$post_id.'">';
+				echo '<input type="submit" name="enable" value="publish">
+				<input type="submit" name="disable" value="unpublish" formaction="unpublish.php"> </form></table> </div>';
+			}
+?> 
 
 </body>
 </html>
---
+++ admin/publish.php
@@ -0,0 +1,17 @@
+<?php
+	include "../includes/db_config.php";
+	include "../includes/db.php";
+	require_once "../includes/session.php";
+
+	// checks if the user logged in is an admin account
+	if($acc_type!=1){
+		header("Location:../login/login.php");
+        exit;
+	}
+
+	$blog = $_GET['postId'];
+
+	$db = new Db();
+			$publish = $db->query("UPDATE `posts` SET `status` = b'1' WHERE `posts`.`post_id` = $blog;");
+	header("Location:blogs.php");
+?>
