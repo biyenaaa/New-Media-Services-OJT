c581d4945bc2f11ecd21ad78e5517b4cfb1b38a3
chaserylle <oneandonlychasey@gmail.com>
2018-06-19 10:34:17 +0800

    #10 functional session requirement

--- admin/homepage.php
+++ admin/homepage.php
@@ -1,10 +1,8 @@
 
 <?php 
-     //require_once "../includes/loginprocess.php";
 	include "../includes/db_config.php";
 	include "../includes/db.php";
-
-	session_start();
+	require_once "../includes/session.php";
 
 	$db = new Db();
 	$author = $db->query("SELECT username, date_registered FROM accounts ORDER BY date_registered DESC;");
--- includes/session.php
+++ includes/session.php
@@ -1,19 +1,24 @@
 <?php
-    require "db_config.php";
+    include "db_config.php";
+    include "db.php";
     session_start();
-
-    if(!isset($_SESSION['acc_id'])){
-        $checkUser = $_SESSION['acc_id'];
-
-        $sessionSql = mysqli_query($connection, "SELECT `acc_id`, `username`, `email`, `password`, `acc_type`, `date_registered`, `status` FROM `accounts` where username = '$checkUser'");
-        $row = mysqli_fetch_array($sessionSql, MYSQLI_ASSOC);
-        $session_user = $row['name'];
-        $acc_id = $row['acc_id'];
-        $username = $row['username'];
-        $acc_type = $row['email'];
-        $password = $row['password'];
-        $acc_type = $row['acc_type'];
-        $date_registered = $row['date_registered'];
-        $status = $row['status'];
-   }
+     echo "<h3>Welcome author <b>".$_SESSION['user_id'] ."</b></h3>";
+        if(isset($_SESSION['user_id'])){
+            $checkUserId = $_SESSION['user_id'];
+            $checkUser = $_SESSION['user_name'];
+                    $db = new Db();
+                    $sessionSql = $db->query("SELECT `acc_id`, `username`, `email`, `password`, `acc_type`, `date_registered`, `status` FROM `accounts` where username = '$checkUser';");
+                    $row = mysqli_fetch_array($sessionSql, MYSQLI_ASSOC);
+                    $acc_id = $row['acc_id'];
+                    $username = $row['username'];
+                    $acc_type = $row['email'];
+                    $password = $row['password'];
+                    $acc_type = $row['acc_type'];
+                    $date_registered = $row['date_registered'];
+                    $status = $row['status'];
+        }
+        else{
+            header("Location:../login/login.php");
+            exit;
+        }
 ?>
