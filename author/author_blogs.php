354be5a79af3914b05e4b289d5477a1a7b982965
lovelynparis <lovelynparis26@gmail.com>
2018-06-20 11:53:07 +0800

    #14 Collapse layout for blog content

--- author/author_blogs.php
+++ author/author_blogs.php
@@ -20,84 +20,64 @@ $db = new Db();
 
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <ul class="nav nav-tabs">
-                <li role="presentation" class="">
-                    <a href="#">Profile</a>
-                </li>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<ul class="nav nav-tabs">
+				<li role="presentation" class="">
+					<a href="#">Profile</a>
+				</li>
 
-                <li role="presentation" class="active">
-                    <a href="author_blogs.php"><span class="glyphicon glyphicon-list-alt"></span> My Blogs</a>
-                </li>
+				<li role="presentation" class="active">
+					<a href="author_blogs.php"><span class="glyphicon glyphicon-list-alt"></span> My Blogs</a>
+				</li>
 
-                <li role="presentation" class="">
-                    <a href="profile.php"><span class="glyphicon glyphicon-plus"></span> Create a New Blog</a>
-                </li>
+				<li role="presentation" class="">
+					<a href="profile.php"><span class="glyphicon glyphicon-plus"></span> Create a New Blog</a>
+				</li>
 
-                <li role="presentation" class="">
-                    <a href="../logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+				<li role="presentation" class="">
+					<a href="../logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
 
 	<div class="container-fluid">
-		<table class="table">
-			<tr>
-				<th>Title</th>
-				<th>Date Published</th>
-				<th>Actions</th>
-			</tr>
-
-
+		<div class="accordion" id="accordionExample">
 			<?php
 			$acc_id = $_SESSION['user_id'];
 			$result = $db -> query("SELECT post_id, title, content, date_published FROM posts WHERE acc_id='$acc_id' ");
 
 			while($row = $result->fetch_assoc()){
+			$post_id = $row['post_id'];
 			$title = $row['title'];
 			$username = $row['content'];
 			$date_published = $row['date_published'];
 			$content = $row['content'];
 
-			$output = 
-			'<tr>
-				<td>'.$title.'</td>
-				<td>'.$date_published.'</td>
-				<td><form action="#" method="GET">
-					<button type="button" class="view" data-toggle="modal" data-target="#viewContent" name="View">View</button>
-					<button type="submit" class="edit" name="Edit"><span class="glyphicon glyphicon-edit"></span> Edit</button>
-					<button type="submit" class="edit" name="Delete"><span class="glyphicon glyphicon-trash"></span> Delete</button>
-				</form></td>
-
-				<div class="modal fade" id="viewContent" role="dialog">
-					<div class="modal-dialog">
-						<!-- Modal content-->
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal">&times;</button>
-								<h4 class="modal-title">'.$title.'</h4>
-							</div>
-							<div class="modal-body">
-								<p>'.$content.'</p>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							</div>
-						</div>
-					</div>
+			$output = '
+			<div class="container">
+				<h4>'.$title.'</h4>
+				<p>'.$date_published'</p>
+				<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">View</button>
+				<div id="demo" class="collapse">
+					'.$content.'
 				</div>
-				';
+			</div>
 
-				echo $output;
-			}
-			?>
-		</tr>
-	</table>
 
-	<br>
+			';
+
+			echo $output;
+		}
+		?>
+
+	</div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" ></script>
 
 </body>
 </html>
