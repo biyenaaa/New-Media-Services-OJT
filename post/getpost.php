0726739775f82bc5f529e5009b20a6ae32783653
chaserylle <oneandonlychasey@gmail.com>
2018-06-21 17:58:28 +0800

    #22 content now available to visitors

--- post/getpost.php
+++ post/getpost.php
@@ -1,7 +1,22 @@
 <?php
 	include "../includes/db_config.php";
 	include "../includes/db.php";
-	include "../includes/session.php";
+	
+	session_start();
+        if(isset($_SESSION['user_id'])){
+            $checkUserId = $_SESSION['user_id'];
+            $checkUser = $_SESSION['user_name'];
+            $db = new Db();
+            $sessionSql = $db->query("SELECT `acc_id`, `username`, `email`, `password`, `acc_type`, `date_registered`, `status` FROM `accounts` where username = '$checkUser';");
+            $row = mysqli_fetch_array($sessionSql, MYSQLI_ASSOC);
+            $acc_id = $row['acc_id'];
+            $username = $row['username'];
+            $email = $row['email'];
+            $password = $row['password'];
+            $acc_type = $row['acc_type'];
+            $date_registered = $row['date_registered'];
+            $status = $row['status'];
+        }
 	include "../modules/navbar.php";
 	include "../modules/footer.php";
 
