835c0c98b8cf1d3a1443149a982f3a50dfb66d3f
chaserylle <oneandonlychasey@gmail.com>
2018-06-21 16:43:21 +0800

    #25 fixes

--- modules/navbar.php
+++ modules/navbar.php
@@ -19,12 +19,14 @@
     <div class="navig"> 
   		<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <?php
+        if(isset($acc_type)){
           if($acc_type=="0"){
             echo '<a class="navbar-brand" href="../index.php">Blog & White</a>';
           }
           else{
             echo '<a class="navbar-brand" href="../admin/homepage.php">Blog & White</a>';
           }
+        }
         ?>
     			
     			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
