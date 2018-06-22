f530ecb8ec0266087c6286b6b9aa07745c467170
chaserylle <oneandonlychasey@gmail.com>
2018-06-22 11:10:44 +0800

    #4 header icon

--- modules/navbar.php
+++ modules/navbar.php
@@ -10,6 +10,7 @@
 		<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../style/images/logo.png">
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
